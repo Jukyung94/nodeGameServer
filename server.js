05f6b387c8c944216e69ce2dedf7b37279b591e3
Jukyung <sungjukyung94@gmail.com>
2023-07-23 00:10:28 +0900

    add bodyparser, connect with unity_230723

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const db_donfig = require(__dirname + '/db.js');
 const conn = db_donfig.init();
 
 var app = express();
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+  console.log('hello world');
+  res.send('<h1>hello world</h1>');
+}) 
 
 app.get('/new', (req, res) => {
   conn.query('SELECT * FROM user', function(err, result, fields) {
@@ -14,6 +21,23 @@ app.get('/new', (req, res) => {
     res.end();
   });
 });
+
+app.post('/add', (req, res) => {
+  console.log('11111111111111111111111111111111');
+  console.log(req.body);
+  const name = req.body.userName;
+  const email = req.body.userId;
+  const queryString = `INSERT INTO user (userName, email, password) VALUES ('${name}', '${email}', '"1234"')`;
+  conn.query(queryString, function(err, result) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log('user Info insert successfully');
+    }
+  })
+  res.json("asdf");
+
+})
 
 app.listen(8080, () => {
   console.log('server start');
